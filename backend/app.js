ad07dab58d4523c08850273993752d2bfffd6b19
Frizon Mahalia <mahalia.frizon@gmail.com>
2023-02-16 11:04:39 +0100

    ajout dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,32 +1,41 @@
-const express = require('express'); // installation d'express
-const app = express(); // création de l'application express
-const mongoose = require('mongoose'); //importation de mongoose
+ // import d'express
+const express = require('express');
+// création de l'application express
+const app = express(); 
+ //importation de mongoose
+const mongoose = require('mongoose');
+//import du path du server
 const path = require('path');
+//import des routes
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require ('./routes/user');
 
+//Importation de dotenv pour les variables d'environement :
+const dotenv = require("dotenv");
+dotenv.config();
 
-
+//connexion à MongoDB Atlas
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb+srv://Caracole13:PqMnGd17@cluster0.qi2aeio.mongodb.net/test',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
-  .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'))
 
-
-app.use((req, res, next) => { //definition de headers spécifiques pour permettre des requêtes cross.origin
+//definition de headers spécifiques pour permettre des requêtes cross.origin - CORS
+app.use((req, res, next) => { 
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
 
+//permet de récupérer des requêtes et de les afficher au format json
+app.use(express.json()); 
 
-app.use(express.json()); //équivalent bodyparser
-
+//ROUTES
 app.use('/api/sauces', sauceRoutes);
 app.use('/api/auth', userRoutes);
 app.use('/images', express.static(path.join(__dirname,'images')));
 
+//Export du fichier app.js
 module.exports = app;
--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -76,3 +76,68 @@ exports.deleteSauce = (req, res, next) => {
         res.status(500).json({ error });
     });
 };
+
+// User is liking / disliking a sauce
+exports.likeSauce = (req, res, next) => {
+  const sauceId = req.params.id;
+  const userId = req.body.userId;
+  const like = req.body.like;
+  // 1. user likes a sauce for the first time (like === 1)
+  // pushing the userId to usersLiked array; incrementing likes
+  if (like === 1) {
+    Sauce.updateOne(
+      { _id: sauceId },
+      {
+        $inc: { likes: like },
+        $push: { usersLiked: userId },
+      }
+    )
+      .then((sauce) => res.status(200).json({ message: "Sauce appréciée" }))
+      .catch((error) => res.status(500).json({ error }));
+  }
+
+  // 2. user DISlikes a sauce for the first time (like === -1)
+  // pushing the userId to usersLiked array; one less like.
+  else if (like === -1) {
+    Sauce.updateOne(
+      { _id: sauceId },
+      {
+        $inc: { dislikes: -1 * like },
+        $push: { usersDisliked: userId },
+      }
+    )
+      .then((sauce) => res.status(200).json({ message: "Sauce dépréciée" }))
+      .catch((error) => res.status(500).json({ error }));
+  }
+  // 3. User changes his mind
+  // 3.1. user is taking back his like :
+  else {
+    Sauce.findOne({ _id: sauceId })
+      .then((sauce) => {
+        if (sauce.usersLiked.includes(userId)) {
+          Sauce.updateOne(
+            { _id: sauceId },
+            { $pull: { usersLiked: userId }, $inc: { likes: -1 } }
+          )
+            .then((sauce) => {
+              res.status(200).json({ message: "Sauce dépréciée" });
+            })
+            .catch((error) => res.status(500).json({ error }));
+          // 3.2 user is changing his mind on his dislike
+        } else if (sauce.usersDisliked.includes(userId)) {
+          Sauce.updateOne(
+            { _id: sauceId },
+            {
+              $pull: { usersDisliked: userId },
+              $inc: { dislikes: -1 },
+            }
+          )
+            .then((sauce) => {
+              res.status(200).json({ message: "Sauce appréciée" });
+            })
+            .catch((error) => res.status(500).json({ error }));
+        }
+      })
+      .catch((error) => res.status(401).json({ error }));
+  }
+};
